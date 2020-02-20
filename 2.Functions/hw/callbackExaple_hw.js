b06787ffb9c08732ab9767afd52083922a1739b3
jsw0402 <jsw0402jyw@kpu.ac.kr>
2020-02-20 13:56:46 +0900

    Fix functions

--- 2.Functions/hw/callbackExaple_hw.js
+++ 2.Functions/hw/callbackExaple_hw.js
@@ -1,16 +1,15 @@
 list=[1,2,3,4,5,6,7,8,9,10];
 
-let add=(items,callback)=>{
-    setTimeout(()=>
-    {
-        let sum=items.reduce((a,b)=>(a+b));
-        callback(sum);
+let callbackExample=(items,callback)=>{
+    setTimeout(()=>{
+    let sum=items.reduce((a,b)=>a+b);
+    callback(sum);
     },0);
-}
+};
 
-add(list,result=>{
+callbackExample(list,result=>{
     console.log(result);
 });
 
-console.log("first")
+console.log("first");
 

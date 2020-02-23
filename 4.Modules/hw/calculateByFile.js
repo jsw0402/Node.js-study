e3a27bbceba603132b0d95ce5bd636e6d270b7fa
jsw0402 <jsw0402jyw@kpu.ac.kr>
2020-02-23 14:43:47 +0900

    Fix Modules

--- 4.Modules/hw/calculateByFile.js
+++ 4.Modules/hw/calculateByFile.js
@@ -9,16 +9,16 @@ fs.readFile('./4.Modules/hw/input.txt',(err,data)=>{
     let words=data.split(',');
     let result;
     if (words[1]=='+'){
-        result=calc.add(Number(words[0]),Number(words[2]));
+        result=calc.add(words[0],words[2]);
     }
     else if (words[1]=='-'){
-        result=calc.sub(Number(words[0]),Number(words[2]));
+        result=calc.sub(words[0],words[2]);
     }
     else if (words[1]=='*'){
-        result=calc.mul(Number(words[0]),Number(words[2]));
+        result=calc.mul(words[0],words[2]);
     }
     else if (words[1]=='/'){
-        result=calc.div(Number(words[0]),Number(words[2]));
+        result=calc.div(words[0],words[2]);
     };
     const writeFile=fs.writeFile('./4.Modules/hw/output.txt',result,(err)=>{
         if (err){
